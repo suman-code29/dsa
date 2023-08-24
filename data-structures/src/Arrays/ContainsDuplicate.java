7698d441dd48e4521dad3b3193400a2f95079148
sumank <saisuman@joveo.com>
2023-08-24 19:30:59 +0530

    array - contains duplicate

---
+++ data-structures/src/Arrays/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> existingValues = new HashSet<>();
+        for (int num : nums) {
+            if (existingValues.contains(num)) {
+                return true;
+            } else {
+                existingValues.add(num);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        boolean result = containsDuplicate.containsDuplicate(nums);
+        System.out.println(result);
+    }
+}
