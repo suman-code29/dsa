e5191f6306e9ee808cfe190b6e06e8bf658b2d99
Sai Suman K <suman@Sumans-MacBook-Air.local>
2023-10-30 01:39:23 +0530

    missing numbers problem

---
+++ data-structures/src/Arrays/MissingNumber.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        Set<Integer> numberRange = new HashSet<>();
+        for(int i = 0; i<=nums.length;i++) {
+            numberRange.add(i);
+        }
+        for (int num : nums) {
+            if (numberRange.contains(num)) {
+                numberRange.remove(num);
+            }
+        }
+        for (int num : numberRange){
+            return num;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{9,6,4,2,3,5,7,0,1};
+        MissingNumber missingNumber = new MissingNumber();
+        System.out.println(missingNumber.missingNumber(nums));
+    }
+}
