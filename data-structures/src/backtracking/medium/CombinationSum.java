220494b8019775a17918430f74cb82a7dd1c54cf
ash <suman@Sumans-personal-mac.local>
2024-07-02 16:29:06 +0530

    back tracking problems.

--- data-structures/src/backtracking/medium/CombinationSum.java
+++ data-structures/src/backtracking/medium/CombinationSum.java
@@ -5,26 +5,26 @@ import java.util.stream.Collectors;
 
 public class CombinationSum {
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> tempResult = new ArrayList<>();
-        solve(result, tempResult, 0, candidates, target);
-        return result;
-    }
-
-    public void solve(List<List<Integer>> result, List<Integer> tempResult, int idx, int[] candidates, int target) {
-        if (target == 0) {
-            result.add(new ArrayList<>(tempResult));
-        } else if (target < 0 || idx >= candidates.length) {
-            return;
-        } else {
-            tempResult.add(candidates[idx]);
-            solve(result, tempResult, idx, candidates, target - candidates[idx]);
-
-            tempResult.remove(tempResult.size()-1);
-            solve(result, tempResult, idx+1, candidates, target);
-        }
-    }
+//    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        List<Integer> tempResult = new ArrayList<>();
+//        solve(result, tempResult, 0, candidates, target);
+//        return result;
+//    }
+//
+//    public void solve(List<List<Integer>> result, List<Integer> tempResult, int idx, int[] candidates, int target) {
+//        if (target == 0) {
+//            result.add(new ArrayList<>(tempResult));
+//        } else if (target < 0 || idx >= candidates.length) {
+//            return;
+//        } else {
+//            tempResult.add(candidates[idx]);
+//            solve(result, tempResult, idx, candidates, target - candidates[idx]);
+//
+//            tempResult.remove(tempResult.size()-1);
+//            solve(result, tempResult, idx+1, candidates, target);
+//        }
+//    }
 
     public static void main(String[] args) {
         CombinationSum combinationSum = new CombinationSum();
@@ -32,5 +32,25 @@ public class CombinationSum {
                         .map(String::valueOf)
                         .collect(Collectors.joining(", ")))
                 .forEach(System.out::println);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+
+        dfs(result, current, 0, candidates, target);
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, List<Integer> current, int idx, int[] candidates, int target) {
+        if (target == 0) {
+            result.add(new ArrayList<>(current));
+        } else if (target < 0 || idx >= candidates.length) {
+        } else {
+            current.add(candidates[idx]);
+            dfs(result, current, idx, candidates, target-candidates[idx]);
+            current.remove(current.size()-1);
+            dfs(result, current, idx+1, candidates, target);
+        }
     }
 }
--- data-structures/src/backtracking/medium/SubsetsII.java
+++ data-structures/src/backtracking/medium/SubsetsII.java
@@ -7,52 +7,40 @@ public class SubsetsII {
 //    public List<List<Integer>> subsetsWithDup(int[] nums) {
 //        List<List<Integer>> result = new ArrayList<>();
 //        List<Integer> current = new ArrayList<>();
+//        Arrays.sort(nums);
 //        solve(result, current, 0, nums);
-//        List<List<Integer>> sortedLists = result.stream()
-//                .map(innerList -> {
-//                    List<Integer> sortedList = new ArrayList<>(innerList);
-//                    Collections.sort(sortedList);
-//                    return sortedList;
-//                })
-//                .toList();
-//
-//        Set<List<Integer>> uniqueSortedLists = new HashSet<>(sortedLists);
-//
-//        return new ArrayList<>(uniqueSortedLists);
+//        return result;
 //    }
 //
 //    private void solve(List<List<Integer>> result, List<Integer> current, int idx, int[] nums) {
-//        if (idx >= nums.length) {
-//            result.add(new ArrayList<>(current));
-//            return;
+//        result.add(new ArrayList<>(current));
+//        for (int i=idx; i<nums.length; i++) {
+//            if (i>idx && nums[i] == nums[i-1]) continue;
+//            current.add(nums[i]);
+//            solve(result, current, i+1, nums);
+//            current.remove(current.size()-1);
 //        }
-//        current.add(nums[idx]);
-//        solve(result, current, idx+1, nums);
-//
-//        current.remove(current.size()-1);
-//        solve(result, current, idx+1, nums);
 //    }
-
+    public static void main(String[] args) {
+        SubsetsII subsetsII = new SubsetsII();
+        System.out.println(subsetsII.subsetsWithDup(new int[]{1,2,2}));
+    }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
         Arrays.sort(nums);
-        solve(result, current, 0, nums);
+        dfs(result,current, nums, 0);
         return result;
     }
 
-    private void solve(List<List<Integer>> result, List<Integer> current, int idx, int[] nums) {
+    private void dfs(List<List<Integer>> result, List<Integer> current, int[] nums, int idx) {
         result.add(new ArrayList<>(current));
-        for (int i=idx; i<nums.length; i++) {
+        for (int i=idx; i<nums.length ; i++) {
             if (i>idx && nums[i] == nums[i-1]) continue;
             current.add(nums[i]);
-            solve(result, current, i+1, nums);
+            dfs(result, current, nums, i+1);
             current.remove(current.size()-1);
         }
-    }
-    public static void main(String[] args) {
-        SubsetsII subsetsII = new SubsetsII();
-        System.out.println(subsetsII.subsetsWithDup(new int[]{1,2,2}));
     }
 }
