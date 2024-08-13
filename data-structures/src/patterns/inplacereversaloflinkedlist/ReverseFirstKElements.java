fc7306cd6ba4dcb22aef5592885e729c4ae11e1e
Kamalapuram Sai Suman Goud <kamalapuramsaisuman_goud@epam.com>
2024-08-13 22:35:26 +0530

    changes

--- data-structures/src/patterns/inplacereversaloflinkedlist/ReverseFirstKElements.java
+++ data-structures/src/patterns/inplacereversaloflinkedlist/ReverseFirstKElements.java
@@ -1,4 +1,16 @@
 package patterns.inplacereversaloflinkedlist;
 
 public class ReverseFirstKElements {
+    static class ListNode {
+        int val;
+        ReverseLinkedList.ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode reverseFirstKElements(ListNode head) {
+        return head;
+    }
 }
